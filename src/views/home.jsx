b68a0e5bd3c713b0ae3ea356b01a03348c153a10
Sergio <checcarelli.s@gmail.com>
2024-10-21 15:28:33 -0300

    integration validate doctor

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -213,9 +213,15 @@ const Home = () => {
       : dataBookings?.length > 0 && verifyTurnsAvailable(dataBookings)
       ? "Tienes turnos proximos"
       : "Sin turnos próximos.";
+
   const welcomeText =
-    (user.role === "DOCTOR" || user.role === "ADMIN") &&
-    dataBookings?.length === 0
+    (user.role === "DOCTOR" && user.validated === "incompleted")
+      ? "<p>Por favor complete sus datos para realizar la validación de sus datos.</p>"
+      : (user.role === "DOCTOR" && user.validated === "pending")
+      ? "<p>Su validación está pendiente. Por favor, espere la confirmación.</p>"
+      : (user.role === "DOCTOR" && user.validated === "disabled")
+      ? "<p>Lo sentimos, pero usted no se encuentra habilitado. Por favor revise los datos ingresados.</p>"
+      : (user.role === "DOCTOR" || user.role === "ADMIN") && dataBookings?.length === 0
       ? "<p>Por el momento no tiene turnos agendados.</p>"
       : user.role === "PACIENTE" && dataBookings?.length === 0
       ? "Encuentra al médico que necesitas y programa tu cita en solo unos pasos. <b>¡Tu atención pediátrica está a solo cuatro pasos de distancia!</b>"
@@ -282,15 +288,17 @@ const Home = () => {
               order={user.role === "DOCTOR" || user.role === "ADMIN" ? 2 : 1}
               gap={4}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                style={{ width: "52px", height: "56px" }}
-                fill="#104DBA"
-              >
-                <path d="M128 0c13.3 0 24 10.7 24 24V64H296V24c0-13.3 10.7-24 24-24s24 10.7 24 24V64h40c35.3 0 64 28.7 64 64v16 48V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V192 144 128C0 92.7 28.7 64 64 64h40V24c0-13.3 10.7-24 24-24zM400 192H48V448c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V192zm-95 89l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
-              </svg>
-              {(user.role === "DOCTOR" || user.role === "ADMIN") &&
+              {!(user.role === "DOCTOR" && !(user.validated === "completed")) && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                  style={{ width: "52px", height: "56px" }}
+                  fill="#104DBA"
+                >
+                  <path d="M128 0c13.3 0 24 10.7 24 24V64H296V24c0-13.3 10.7-24 24-24s24 10.7 24 24V64h40c35.3 0 64 28.7 64 64v16 48V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V192 144 128C0 92.7 28.7 64 64 64h40V24c0-13.3 10.7-24 24-24zM400 192H48V448c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V192zm-95 89l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+                </svg>
+              )}
+              {((user.role === "DOCTOR") && (user.validated === "completed") || user.role === "ADMIN") &&
                 dataBookings?.length === 0 && (
                   <Text
                     fontSize={["26px", "48px"]}
